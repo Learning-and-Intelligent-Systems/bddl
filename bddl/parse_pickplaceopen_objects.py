b9595931cfbd6858b4aecee1010e23c4e3cc8ce9
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-09-06 12:10:46 -0400

    tidies up parser * also separates objects we can place on vs. into

--- bddl/parse_pickplaceopen_objects.py
+++ bddl/parse_pickplaceopen_objects.py
@@ -7,21 +7,29 @@ import json
 
 objs_to_place = set()
 objs_to_place_into = set()
+objs_to_place_ontop = set()
+# Parse all PDDL files to find all objects that are mentioned in
+# inside(), under() and ontop() predicates. The first object mentioned
+# in these predicates must be graspable and the second must be a surface
+# that can be placed into, under or ontop of.
 for filename in glob.glob("bddl/activity_definitions/*/*.pddl"):
     with open(filename, "r") as f:
         for line in f.readlines():
             line = line.lstrip()
             if len(line) != 0 and line[0] == "(":
+                # If we see inside, under or ontop, that's an object that we must be
+                # able to pick.
                 if True in [pred in line[1:] for pred in ["inside ", "under ", "ontop "]]:
                     predicate_obj_list = line[1:].replace("(", "").replace(")", "").replace("?", "").replace("\n", "").split(" ")
                     objs_to_place.add(predicate_obj_list[1].split(".")[0])
-                    objs_to_place_into.add(predicate_obj_list[2].split(".")[0])
+                    # Parse objects that we can place into or ontop of/under separately.
+                    if "inside " in line[1:]:
+                        objs_to_place_into.add(predicate_obj_list[2].split(".")[0])
+                    else:
+                        objs_to_place_ontop.add(predicate_obj_list[2].split(".")[0])
 
-print(objs_to_place)
-print()
-print(objs_to_place_into)
-print()
-
+# Now, given each of these object sets, find all sub-types in the type-hierarchy JSON
+# to add to these sets.
 def get_all_children(object_dict):
     if "children" not in object_dict.keys():
         return []
@@ -47,6 +55,7 @@ with open("bddl/hierarchy_all.json", "r") as f:
 
 objs_to_place_extended = set()
 objs_to_place_into_extended = set()
+objs_to_place_ontop_extended = set()
 
 for obj_name in objs_to_place:
     objs_to_place_extended |= set(get_all_children(get_object_type_hierarchy_by_name(obj_name, data)))
@@ -54,25 +63,16 @@ for obj_name in objs_to_place:
 for obj_name in objs_to_place_into:
     objs_to_place_into_extended |= set(get_all_children(get_object_type_hierarchy_by_name(obj_name, data)))
 
-print(objs_to_place_extended)
-print()
-print(objs_to_place_into_extended)
-print()
+for obj_name in objs_to_place_ontop:
+    objs_to_place_ontop_extended |= set(get_all_children(get_object_type_hierarchy_by_name(obj_name, data)))
 
-print(len(objs_to_place))
-print()
-print(len(objs_to_place_into))
-print()
-print(len(objs_to_place_extended))
-print()
-print(len(objs_to_place_into_extended))
-print()
-
-print(objs_to_place | objs_to_place_extended)
-print(objs_to_place_into | objs_to_place_into_extended)
-
+# Combine the extended sets with the original.
+objs_to_place |= objs_to_place_extended
+objs_to_place_into |= objs_to_place_into_extended
+# Add all surfaces to objects to place onto.
 surfaces = set(get_all_children(get_object_type_hierarchy_by_name("surface", data)))
-print(objs_to_place_into | objs_to_place_into_extended | surfaces)
+objs_to_place_ontop |= objs_to_place_ontop_extended | surfaces
+
 
 # Function to parse out objects that have an 'openable' property.
 def get_all_openable_children(object_dict):
@@ -86,6 +86,14 @@ def get_all_openable_children(object_dict):
 
 # Print the result of the get_all_openable_children
 # function.
+print("GRASPABLE OBJECTS:")
+print(objs_to_place)
+print()
+print("SURFACES WE CAN PLACE INTO:")
+print(objs_to_place_into)
+print()
+print("SURFACES WE CAN PLACE ONTO:")
+print(objs_to_place_ontop)
 print()
 print("OPENABLE OBJECTS:")
 print(get_all_openable_children(data))
